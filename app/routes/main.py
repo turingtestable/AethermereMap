ae855fc8633aaff7a3178b04eca76dfc735ea5b5
Jake Hilty <jacob.a.hilty@gmail.com>
2025-09-22 16:18:51 -0400

    Added simple auth

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 from app.models import District
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
+@login_required
 def index():
     districts = District.query.order_by(District.district_number).all()
     return render_template('index.html', districts=districts)
